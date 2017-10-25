6db1f465b8bcf8f1cf85a4feeb282aa993a8a20f
Corey Redding <coreysnightout@gmail.com>
2017-10-25 15:08:59 -0700

    push test

---
+++ react-redux-course/src/reducers/player.js
@@ -0,0 +1,20 @@
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState = [
+  {
+    name: 'Jim Hoskins',
+    score: 31,
+  },
+  {
+    name: 'Andrew Chalkley',
+    score: 20,
+  },
+  {
+    name: 'Alena Holligan',
+    score: 50,
+  },
+];
+
+export default function Player(state=initialState, action) {
+  
+}
